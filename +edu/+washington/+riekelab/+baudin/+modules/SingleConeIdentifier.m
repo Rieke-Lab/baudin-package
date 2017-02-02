be4e1baf8a60a7310660506c77e9d84b11bffe28
Jacob Baudin <jacob.baudin@gmail.com>
2017-02-02 10:48:31 -0800

    starting cone identification module

---
+++ +edu/+washington/+riekelab/+baudin/+modules/SingleConeIdentifier.m
@@ -0,0 +1,19 @@
+classdef SingleConeIdentifier < symphony.ui.Module
+    properties (Access = private)
+        
+    end
+    
+    properties (Constant)
+        
+    end
+    
+    methods
+        function createUi(obj, figureHandle)
+            import appbox.*;
+            
+            set(figureHandle, ...
+                'Name', 'Single Cone Identifier', ...
+                'Position', 
+        end
+    end
+end
